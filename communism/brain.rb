27f9471cab66627293b4dc9fa63985e02af3f01f
walmart-givenchii <joesgot2bcool@gmail.com>
2017-06-10 12:42:37 -0700

    dded commie

---
+++ communism/brain.rb
@@ -0,0 +1,9 @@
+require 'discordrb'
+#this makes commies feel cool
+bot = Discordrb::Bot.new token: 'MzE5MzA2NjM3Njc4NzM5NDg4.DA_BBA.1JUsNyW49ypR_OIepVnoqDQTlEQKzSm230', client_id: 319306637678739488
+
+bot.message(with_text: 'spectre') do |event|
+  event.respond '"A spectre is haunting Europe – the spectre of communism. All the powers of old Europe have entered into a holy alliance to exorcise this spectre: Pope and Tsar, Metternich and Guizot, French Radicals and German police-spies. Where is the party in opposition that has not been decried as communistic by its opponents in power? Where is the opposition that has not hurled back the branding reproach of communism, against the more advanced opposition parties, as well as against its reactionary adversaries.Two things result from this fact: I. Communism is already acknowledged by all European powers to be itself a power. II. It is high time that Communists should openly, in the face of the whole world, publish their views, their aims, their tendencies, and meet this nursery tale of the Spectre of Communism with a manifesto of the party itself.To this end, Communists of various nationalities have assembled in London and sketched the following manifesto, to be published in the English, French, German, Italian, Flemish and Danish languages." - Marx and Engels, from the Communist Manifesto.'
+end
+
+bot.run
